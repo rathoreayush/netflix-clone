1b9aa6a210c5fd329a0a19c2b392a96fa8c9dd5f
Ayush Singh <rathoreayush96081@gmail.com>
2023-01-25 01:09:57 +0530

    comit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //Definig constant, Api key, ApiEnd Point, Api Path
 const apikey="6f526342750030370855e91170a08827";
 const apiEndPoint= "https://api.themoviedb.org/3";
+const imgPath= "https://image.tmdb.org/t/p/original"
 const apiPaths={
     fetchAllCatogories : `${apiEndPoint}/genre/movie/list?api_key=${apikey}`,
     fetchMovieList:(id)=>`${apiEndPoint}/discover/movie?api_key=${apikey}`,
@@ -34,10 +35,39 @@ function fetchAndBuildMovieSection(fetchUrl,categories){
     .then(res=>res.json())
     .then(res=>{
         console.table(res.results);
+        const movies=res.results;
+        if(Array.isArray(categories)&& categories.length){
+            buildMoviesSection(movies,categories.name);
+        }
     })
     .catch(err=>console.log(err))
+} 
+// calling another function to print html list
+function buildMoviesSection(list,categoryName){
+   const movieCont=document.getElementById("movies-cont");
+   const movieListHtml=list.map(item=>{
+    return    ` <img class="movie-item" src="${imgPath}${item.backdrop_path}" alt="${item.title}">`
+   }).join('');
+
+   const movieSectionHtml= `
+   
+<h2 class="movie-section-heading">${categoryName} <span class="explore-nudge">Explore All</span></h2>
+<div class="movie-row">
+${movieListHtml}
+
+</div>
+   `
+   console.log(movieSectionHtml);
+   const div=document.createElement('div');
+   div.className="movies-section"
+   div.className=movieSectionHtml;
+
+
+   // Append html into container
+   movieCont.append(div);
 }
 
 window.addEventListener('load',function(){
     init();
 })
+
